6a4b481da17c077c1e3f99dbd0b02d5e2f4dec13
Leonardo <leo.lisita@hotmail.com>
2025-09-18 18:50:26 -0300

    os arquivos cvm não mantém as mesmas colunas ao longo do tempo- estudando como resolver

--- scripts_dados_cvm/data_preparation/dos2unix-v1.sh
+++ scripts_dados_cvm/data_preparation/dos2unix-v1.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# filepath: /home/leo/linguagens/fundos_v3/scripts_dados_cvm/data_preparation/dos2unix-v1.sh
+
+# Caminho relativo a partir do diretório raiz do projeto
+DADOS_DIR="dados"
 
 # Remove \r do final das linhas e todas as aspas simples e duplas de todos os arquivos na pasta dados/ recursivamente
-find "$(dirname "$0")/../dados" -type f -exec sh -c '
+find "$DADOS_DIR" -type f -exec sh -c '
   echo "Processando: $1"
   sed -i "s/\r\$//; s/[\"'\'']/ /g" "$1"
 ' _ {} \;
